cb45cf9c3125137676a391ca5b79f0310b43e338
Gustavo Conte <gustavoferreiraconte@gmail.com>
2025-05-31 18:41:15 -0300

    Replace img tags with next/image to optimize bundle size

--- models/src/app/minha-conta/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function MinhaContaPage() {
-  return (
-    <div className="container mx-auto py-12 px-4">
-      <h1 className="text-3xl font-bold text-gray-800 mb-8">Minha Conta</h1>
-      <p>Gerencie sua conta aqui.</p>
-    </div>
-  );
-}
