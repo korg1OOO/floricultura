d4bb08111017a3a0e4d3941cedcaa76edaa945fa
Gustavo Conte <gustavoferreiraconte@gmail.com>
2025-05-31 18:53:33 -0300

    Replace img tags with next/image to optimize bundle size

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    unoptimized: false,  // Re-enable optimization
+    unoptimized: false, // Re-enable optimization
     domains: [
       "source.unsplash.com",
       "images.unsplash.com",
@@ -30,6 +30,26 @@ const nextConfig = {
         pathname: "/**",
       },
     ],
+  },
+  webpack(config) {
+    config.optimization.splitChunks = {
+      chunks: 'all', // Split all chunks, including vendor and async
+      maxSize: 2000000, // 2 MiB max per chunk
+      minSize: 500000, // 500 kB minimum size for a chunk to be split
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/, // Split node_modules into separate chunks
+          priority: -10,
+          reuseExistingChunk: true,
+        },
+        default: {
+          minChunks: 2, // Split if a module is used in at least 2 places
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    };
+    return config;
   },
 };
 
