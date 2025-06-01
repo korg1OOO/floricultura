b2b156fd6404f7aef1ab15636f39c44bd1d78891
Enzo Michel Barbosa Feldman <enzomichelfeldman@gmail.com>
2025-06-01 09:44:12 -0300

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,6 +2,8 @@
 const config = {
   plugins: {
     tailwindcss: {},
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}), // Minify CSS in production only
   },
 };
 
